3d2afc1091023ff0e8040ac7aa644c6b842e732e
Carlos Teixeira <carlosteixeira.email@gmail.com>
2017-11-19 13:39:25 +0000

    Upload ficheiros

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var titulo = 'Express Azure'
 
-/* GET home page. */
+/* GET Home */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: titulo });
+});
+
+/* GET Exemplo */
+router.get('/exemplo', function(req, res, next) {
+  res.render('exemplo', { title: titulo });
 });
 
 module.exports = router;
